554ef3639f505f043d1f64e4cebf45ddfeecd2d6
BAEK <100k1@naver.com>
2022-03-01 18:03:33 +0900

    완성본

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,15 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../UserContext';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import io from 'socket.io-client';
+import Messages from './messages/Messages';
+import Input from './input/Input';
+import './Chat.css';
+let socket;
 const Chat = () => {
-    const { user, setUser } = useContext(UserContext)
+    const ENDPT = 'localhost:5000';
 
+    const { user, setUser } = useContext(UserContext);
+    let { room_id, room_name } = useParams();
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        socket = io(ENDPT);
+        socket.emit('join', { name: user.name, room_id, user_id: user._id })
+    }, [])
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+    useEffect(() => {
+        socket.emit('get-messages-history', room_id)
+        socket.on('output-messages', messages => {
+            setMessages(messages)
+        })
+    }, [])
+    const sendMessage = event => {
+        event.preventDefault();
+        if (message) {
+            console.log(message)
+            socket.emit('sendMessage', message, room_id, () => setMessage(''))
+        }
+    }
     return (
-        <div>
-            <h1>Chat {JSON.stringify(user)}</h1>
-            <Link to={'/'}>
-                <button>go to home</button>
-            </Link>
+        <div className="outerContainer">
+            <div className="container">
+                <Messages messages={messages} user_id={user._id} />
+                <Input
+                    message={message}
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
+            </div>
         </div>
     )
 }
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,36 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../UserContext';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import RoomList from './RoomList';
+import io from 'socket.io-client';
+let socket;
 const Home = () => {
-    const { user, setUser } = useContext(UserContext)
-    const rooms = [
-        {
-            name: 'room1',
-            _id: '123'
-        },
-        {
-            name: 'room2',
-            _id: '456'
+    const { user, setUser } = useContext(UserContext);
+    const [room, setRoom] = useState('');
+    const [rooms, setRooms] = useState([]);
+    const ENDPT = 'localhost:5000';
+    useEffect(() => {
+        socket = io(ENDPT);
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
         }
-    ]
-    const setAsJohn = () => {
-        const john = {
-            name: 'John',
-            email: 'john@email.com',
-            password: '123',
-            id: '123'
-        }
-        setUser(john);
+    }, [ENDPT])
+    useEffect(() => {
+        socket.on('output-rooms', rooms => {
+            setRooms(rooms)
+        })
+
+    }, [])
+    useEffect(() => {
+        socket.on('room-created', room => {
+            setRooms([...rooms, room])
+        })
+    }, [rooms])
+    useEffect(() => {
+        console.log(rooms)
+    }, [rooms])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        socket.emit('create-room', room);
+        console.log(room);
+        setRoom('');
+
     }
-    const setAsTom = () => {
-        const tom = {
-            name: 'Tom',
-            email: 'tom@email.com',
-            password: '456',
-            id: '456'
-        }
-        setUser(tom);
+
+    if (!user) {
+        return <Redirect to='/login' />
     }
     return (
         <div>
@@ -39,12 +49,15 @@ const Home = () => {
                     <div className="card blue-grey darken-1">
                         <div className="card-content white-text">
                             <span className="card-title">Welcome {user ? user.name : ''}</span>
-                            <form >
+                            <form onSubmit={handleSubmit}>
                                 <div className="row">
                                     <div className="input-field col s12">
                                         <input
                                             placeholder="Enter a room name"
-                                            id="room" type="text" className="validate" />
+                                            id="room" type="text" className="validate"
+                                            value={room}
+                                            onChange={e => setRoom(e.target.value)}
+                                        />
                                         <label htmlFor="room">Room</label>
                                     </div>
                                 </div>
@@ -52,8 +65,7 @@ const Home = () => {
                             </form>
                         </div>
                         <div className="card-action">
-                            <a href="#" onClick={setAsJohn}>set as John</a>
-                            <a href="#" onClick={setAsTom}>set as Tom</a>
+
                         </div>
                     </div>
                 </div>
@@ -62,9 +74,6 @@ const Home = () => {
                 </div>
             </div>
 
-            <Link to={'/chat'}>
-                <button>go to chat</button>
-            </Link>
         </div>
     )
 }
